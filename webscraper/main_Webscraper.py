9dcd1f797a720e28372e86943d644a260ddff28f
Patrick Dai <patrickdai917@gmail.com>
2022-12-26 17:09:10 -0500

    removed auth

--- webscraper/main_Webscraper.py
+++ webscraper/main_Webscraper.py
@@ -13,8 +13,8 @@ import csv, os
 # CONSTANTS
 DRIVER_PATH = "chromedriver_win32/chromedriver.exe"
 UBER_WEBSITE = "https://m.uber.com/looking?drop%5B0%5D=%7B%22latitude%22%3A33.9450646%2C%22longitude%22%3A-118.4055151%2C%22addressLine1%22%3A%22Los%20Angeles%20International%20Airport%20%28LAX%29%22%2C%22addressLine2%22%3A%221%20World%20Way%2C%20Los%20Angeles%22%2C%22id%22%3A%22c2523974-0f10-358d-8de9-8de58a6abcd1%22%2C%22provider%22%3A%22uber_places%22%2C%22index%22%3A0%7D&pickup=%7B%22latitude%22%3A34.0687758%2C%22longitude%22%3A-118.4439306%2C%22addressLine1%22%3A%22UCLA%22%2C%22addressLine2%22%3A%22405%20Hilgard%20Ave%2C%20Los%20Angeles%22%2C%22id%22%3A%2217035f06-801f-3cfb-87b7-365e04a5e5db%22%2C%22provider%22%3A%22uber_places%22%2C%22index%22%3A0%7D&vehicle=125"
-EMAIL = "speedsayssuii" # YOUR OWN EMAIL
-PASSWORD = "smokeweedeveryday" # YOUR OWN PASSWORD TO GMAIL
+EMAIL = "" # YOUR OWN EMAIL
+PASSWORD = "" # YOUR OWN PASSWORD TO GMAIL
 DATA_FILE = "data.csv"
 
 def RequestRide(url):
