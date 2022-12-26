2d94661308c267cf3521cbad24326d3b3cd930dd
Patrick Dai <patrickdai917@gmail.com>
2022-12-26 17:08:14 -0500

    Added consts to scraper

--- webscraper/main_Webscraper.py
+++ webscraper/main_Webscraper.py
@@ -10,13 +10,17 @@ import time
 from datetime import datetime
 import csv, os
 
-# Website to access to acquire prices from.
-website = "https://m.uber.com/looking?drop%5B0%5D=%7B%22latitude%22%3A33.9450646%2C%22longitude%22%3A-118.4055151%2C%22addressLine1%22%3A%22Los%20Angeles%20International%20Airport%20%28LAX%29%22%2C%22addressLine2%22%3A%221%20World%20Way%2C%20Los%20Angeles%22%2C%22id%22%3A%22c2523974-0f10-358d-8de9-8de58a6abcd1%22%2C%22provider%22%3A%22uber_places%22%2C%22index%22%3A0%7D&pickup=%7B%22latitude%22%3A34.0687758%2C%22longitude%22%3A-118.4439306%2C%22addressLine1%22%3A%22UCLA%22%2C%22addressLine2%22%3A%22405%20Hilgard%20Ave%2C%20Los%20Angeles%22%2C%22id%22%3A%2217035f06-801f-3cfb-87b7-365e04a5e5db%22%2C%22provider%22%3A%22uber_places%22%2C%22index%22%3A0%7D&vehicle=125"
+# CONSTANTS
+DRIVER_PATH = "chromedriver_win32/chromedriver.exe"
+UBER_WEBSITE = "https://m.uber.com/looking?drop%5B0%5D=%7B%22latitude%22%3A33.9450646%2C%22longitude%22%3A-118.4055151%2C%22addressLine1%22%3A%22Los%20Angeles%20International%20Airport%20%28LAX%29%22%2C%22addressLine2%22%3A%221%20World%20Way%2C%20Los%20Angeles%22%2C%22id%22%3A%22c2523974-0f10-358d-8de9-8de58a6abcd1%22%2C%22provider%22%3A%22uber_places%22%2C%22index%22%3A0%7D&pickup=%7B%22latitude%22%3A34.0687758%2C%22longitude%22%3A-118.4439306%2C%22addressLine1%22%3A%22UCLA%22%2C%22addressLine2%22%3A%22405%20Hilgard%20Ave%2C%20Los%20Angeles%22%2C%22id%22%3A%2217035f06-801f-3cfb-87b7-365e04a5e5db%22%2C%22provider%22%3A%22uber_places%22%2C%22index%22%3A0%7D&vehicle=125"
+EMAIL = "speedsayssuii" # YOUR OWN EMAIL
+PASSWORD = "smokeweedeveryday" # YOUR OWN PASSWORD TO GMAIL
+DATA_FILE = "data.csv"
 
 def RequestRide(url):
 
     # Get WebDriver.
-    driver = webdriver.Chrome(executable_path="chromedriver_win32/chromedriver.exe", chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=chrome_options)
     driver.maximize_window()
     driver.get(url)
 
@@ -33,7 +37,7 @@ def RequestRide(url):
         EC.presence_of_element_located((By.XPATH, "//input[@type='email']")),
         EC.presence_of_element_located((By.XPATH, "//span[text()='Next']"))
     )
-    driver.find_element(By.XPATH, "//input[@type='email']").send_keys("speedsayssuii")
+    driver.find_element(By.XPATH, "//input[@type='email']").send_keys(EMAIL)
     driver.find_element(By.XPATH, "//span[text()='Next']").click()
 
     # Enters in password.
@@ -42,7 +46,7 @@ def RequestRide(url):
         EC.presence_of_element_located((By.XPATH, "//span[text()='Next']"))
     )
     driver.refresh()
-    driver.find_element(By.XPATH, "//input[@name='password']").send_keys("smokeweedeveryday")
+    driver.find_element(By.XPATH, "//input[@name='password']").send_keys(PASSWORD)
     driver.find_element(By.XPATH, "//span[text()='Next']").click()
 
     # After Google Auth closes, need to switch back to original window.
@@ -58,7 +62,7 @@ def RequestRide(url):
 
         # Refresh and relaod
         time.sleep(60)
-        driver.get(website)
+        driver.get(UBER_WEBSITE)
 
         # Sometimes the website crashes from reloading or accessing it a lot so idea is to try to get a price 
         # but if there is no price to get, reload the window.
@@ -72,7 +76,6 @@ def RequestRide(url):
             #     EC.presence_of_element_located((By.XPATH, "//button[text()='Change']"))
             # )
             # check_label.click()
-            print("HIHIHIHIHIHHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHI")
             continue
 
         # Get current time and price
@@ -81,7 +84,7 @@ def RequestRide(url):
         info = [current_time, current_price[1:]]
 
         # Write to data.csv with the current time and price.
-        file = open("data.csv", 'a')
+        file = open(DATA_FILE, 'a')
         writer = csv.writer(file)
         writer.writerow(info)
     
